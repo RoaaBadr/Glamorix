5c7c3baecd8153c59fe876d112bd655f4a68bd26
Roaa Mohammed <88007570+RoaaBadr@users.noreply.github.com>
2026-04-13 00:11:21 +0000

    Fix import path for BackToTop component in Layout

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/layout/Navbar";
 import Footer from "../Components/layout/Footer";
-import BackToTop from "../components/layout/BackToTop";
+import BackToTop from "../Components/layout/BackToTop";
 
 const Layout = () => {
     return (
